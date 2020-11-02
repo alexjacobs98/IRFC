8a8cb175e549fff65230c71c6faa1a8424bacf18
alexjacobs98 <alexjacobs98@gmail.com>
2020-11-02 20:28:32 +0000

    Major styles and layout changes

---
+++ includes/header.inc.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="./dist/styles/styles.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>IRFC PLAYER PORTAL</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500;600&family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,66 +1,22 @@
-<?php   require './dist/includes/parts/header.inc.php'; ?>
-
-<?php
-  if (isset($_GET['error'])) {
-    if ($_GET['error'] == 'EmptyFields') {
-      // EmptyFields.
-      echo '
-      <div class="alert alert-danger floater-alert alert-dismissible fade show">
-        <strong>Error!</strong> It Appears some fields were left empty!
-        <button type="button" class="close" data-dismiss="alert">&times;</button>
-      </div>
-      ';
-    }elseif ($_GET['error'] == 'SomethingIsntRight'){
-      //something isnt right
-      echo '
-      <div class="alert alert-danger floater-alert alert-dismissible fade show">
-        <strong>Error!</strong> Something wasn\'t right!
-        <button type="button" class="close" data-dismiss="alert">&times;</button>
-      </div>
-      ';
-    }elseif($_GET['error'] == 'UnknownError') {
-      //php error
-      echo '
-      <div class="alert alert-danger floater-alert alert-dismissible fade show">
-        <strong>Error!</strong> A problem has been occurred while submitting your data.
-        <button type="button" class="close" data-dismiss="alert">&times;</button>
-      </div>
-      ';
-    }elseif($_GET['error'] == 'NotRegistered') {
-      //Email not registered
-      echo '
-      <div class="alert alert-danger floater-alert alert-dismissible fade show">
-        <strong>Error!</strong> There isn\'t an account linked to that email address!
-        <button type="button" class="close" data-dismiss="alert">&times;</button>
-      </div>
-      ';
-    }
-  }
-?>
-
-<?php  if (isset($_SESSION['UserID'])){header("Location: ./dist/pages/menu.php");}?>
-  <div class="login-wrapper">
+<?php include("includes/header.inc.php"); ?>
+  <div class="login-page-container">
     <div class="video-container">
-      <video src="./dist\videos\login-background.mp4" loop muted autoplay poster="">
-      </video>
-    </div>
-    <div class="video-overlay">
-    </div>
-    <div class="fluid-container form-container">
-      <div class="row ">
-        <form id="login-form" class="login-form col-lg-4 col-md-5" autocomplete="on" action="./dist/includes/login.inc.php" method="post">
+      <video autoplay loop muted src="./dist/videos/login-background.mp4"></video>
+      <div class="video-overlay"></div>
+      <div class="login-form-container row">
+        <form class="p-3 pt-5 pb-5 col-lg-4 col-md-6 col-sm-12" action="post">
           <div class="logo-container">
-            <img src="./dist/images/logo.jpg" alt="IRFC 150 YEAR SPECIAL LOGO">
+            <img class="pb-4" src="./dist/images/logo.jpg" alt="">
           </div>
-          <input autocomplete="on" type="email" placeholder="Email" name="login-email" value="">
-          <input autocomplete="on" type="password" placeholder="Password" name="login-password" value="">
-          <button class="login-button" type="submit" form="login-form"  name="login-button">Login</button>
+          <input type="email" placeholder="EMAIL" name="login-email">
+          <input type="password" placeholder="PASSWORD" name="login-password">
+          <button class="btn btn-default">LOGIN</button>
           <div class="helper-links">
-            <a href="#">Forgotten Password?</a>
-            <a href=".\dist/pages/register.php">Register</a>
+            <a href="">FORGOTTEN PASSWORD</a>
+            <a href="">REGISTER</a>
           </div>
         </form>
       </div>
     </div>
-  </div>
-<?php require './dist/includes/parts/footer.inc.php'; ?>
+  </div> 
+<?php include("includes/footer.inc.php"); ?>
